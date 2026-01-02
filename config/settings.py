1120b7d5b00e731c5ad05b13effcee53f779ff3f
lucky2007pro <usa20070302@gmail.com>
2026-01-02 22:57:03 +0500

    Render uchun C++ kompyutsiya buyrug'i qo'shildi

--- config/settings.py
+++ config/settings.py
@@ -76,8 +76,10 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 AWS_S3_ENDPOINT_URL = os.environ.get('AWS_S3_ENDPOINT_URL')
+# Supabase S3 bilan ishlashi uchun juda muhim sozlamalar
 AWS_S3_ADDRESSING_STYLE = "path"
-AWS_S3_FILE_OVERWRITE = False
+AWS_S3_REGION_NAME = 'eu-central-1' # Yoki loyihangiz regioni
+AWS_S3_SIGNATURE_VERSION = 's3v4'AWS_S3_FILE_OVERWRITE = False
 AWS_S3_CUSTOM_DOMAIN = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
